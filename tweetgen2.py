66d4c3cbffbe3225c49ff0e631d32371e2ef5ed4
NISH1001 <nishanpantha@gmail.com>
2019-05-09 09:31:35 +0545

    Use seed word to generate tweet

--- tweetgen2.py
+++ tweetgen2.py
@@ -11,13 +11,13 @@ class MarkovChain:
     def __init__(self, lookback=2):
         self.trie = defaultdict(lambda : defaultdict(int))
         self.lookback = lookback
-        self.lines = []
+        self.tweets = []
 
     def train(self, lines):
         """
             Build markov model
         """
-        self.lines += lines
+        self.tweets += lines
         print("Building trie...")
         for title in lines:
             tokens = title.split()
@@ -47,37 +47,39 @@ class MarkovChain:
                 next_word = k
         return next_word
 
-    def generate(self, n):
-        sentences = []
-        while len(sentences) < n:
-            sentence = []
+    def generate(self, initial_words=[]):
+        sentence = initial_words[:-1]
+        if not initial_words:
             next_word = self._sample(self.trie[''].items())
-            while next_word != '':
-                sentence.append(next_word)
-                next_word = self._sample(self.trie[' '.join(sentence[-self.lookback:])].items())
-            sentence = ' '.join(sentence)
-            flag = True
-            for title in self.lines: #Prune lines that are substrings of actual lines
-                if sentence in title:
-                    flag = False
-                    break
-            if flag:
-                sentences.append(sentence)
-        return sentences
+        else:
+            next_word = initial_words[-1]
+        while next_word != '':
+            sentence.append(next_word)
+            next_word = self._sample(self.trie[' '.join(sentence[-self.lookback:])].items())
+        sentence = ' '.join(sentence)
+        return sentence
+        # flag = True
+
+        # # Prune lines that are substrings of actual lines
+        # for tweet in self.tweets:
+        #     if sentence in tweet:
+        #         flag = False
+        #         break
+        # if flag:
+        #     sentences.append(sentence)
+        # return sentences
 
 
 def main():
     tweetfile = "data/tweets/clean/clean.csv"
-    df = load_df(tweetfile)
-    # text = "\n".join(df['text'].values.tolist()).strip()
+    # df = load_df(tweetfile)
     mc = MarkovChain(lookback=2)
-    mc.train(df['text'].values.tolist())
+    mc.train(load_df(tweetfile)['text'].values.tolist())
 
-    n = 1
-    tweets = mc.generate(n)
-    for tweet in tweets:
-        print(tweet)
-
+    # initial_words = ['we', 'tend', 'to']
+    initial_words = ['i', 'think']
+    tweet = mc.generate(initial_words)
+    print(tweet)
 
 if __name__ == "__main__":
     main()
